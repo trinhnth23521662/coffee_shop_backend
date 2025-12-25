a6fe9e437c4809a8f118e973e7c2daeb086fe193
thudinh_23521534 <23521534@gm.uit.edu.vn>
2025-12-26 00:47:25 +0700

    Create models.py

---
+++ tables/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Ban(models.Model):
+    ma_ban = models.AutoField(primary_key=True)
+    ten_ban = models.CharField(max_length=100)
+    trang_thai = models.CharField(max_length=20, default='Trống')
+
+    class Meta:
+        db_table = 'Ban'
+        managed = False
+        app_label = 'tables'
