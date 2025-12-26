208f126b6076f09f2397430b2e002a21fea4998f
Tiem <23521566@gm.uit.edu.vn>
2025-12-26 15:47:46 +0700

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('thanh-toan/', views.api_thanh_toan, name='api_thanh_toan'),
     path('<int:ma_dh>/', views.api_xem_don, name='api_xem_don'),
     path('', views.api_danh_sach_don, name='api_danh_sach_don'),
+    path("tao_don_online/", views.api_tao_don_online),
+    path("them_mon_online/", views.api_them_chi_tiet_online),
+    path("xem_don_online/", views.api_xem_don_online),
+    path("lich_su_don/", views.api_lich_su_don_online),
 ]
