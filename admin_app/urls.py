c6b6acc3962b4a4b85e7a36dfc39354ac5851e3e
trinhnth23521662 <23521662@gm.uit.edu.vn>
2025-12-28 23:20:29 +0700

    Add files via upload

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -3,6 +3,6 @@ from .views import EmployeeAPIView, AdminReportAPIView
 
 urlpatterns = [
     path('employees/', EmployeeAPIView.as_view()),
-    path('employees/<int:pk>/', EmployeeAPIView.as_view()),
+    path('employees/<int:id>/', EmployeeAPIView.as_view()),
     path('reports/', AdminReportAPIView.as_view()),
 ]
