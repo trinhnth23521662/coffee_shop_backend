53b947ea824f5e6e23c32eac8ae12fbd5109777d
thudinh_23521534 <23521534@gm.uit.edu.vn>
2025-12-29 18:22:45 +0700

    Update urls.py

--- promotions/urls.py
+++ promotions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import PromotionAPIView, PromotionDetailAPIView
 
 urlpatterns = [
-    path('promotions/', PromotionAPIView.as_view()),
-    path('promotions/<int:ma_km>/', PromotionDetailAPIView.as_view()),
+    # API cho chức năng: Xem danh sách và Tạo khuyến mãi
+    path('promotions/', PromotionAPIView.as_view(), name='promotion-list-create'),
+
+    # API cho chức năng: Xem chi tiết, Sửa, Xóa khuyến mãi
+    path('promotions/<int:ma_km>/', PromotionDetailAPIView.as_view(), name='promotion-detail'),
 ]
