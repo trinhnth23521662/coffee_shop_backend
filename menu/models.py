6dc76a1c89f6f5eb5e5e6fbe1dad46d1effee270
thudinh_23521534 <23521534@gm.uit.edu.vn>
2025-12-29 17:07:41 +0700

    Update models.py

--- menu/models.py
+++ menu/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class LoaiSP(models.Model):
-    ma_loaisp = models.AutoField(primary_key=True, db_column='MaLoaiSP')
-    ten_loaisp = models.CharField(max_length=150, db_column='TenLoaiSP')
+    ma_loaisp = models.AutoField(primary_key=True, db_column='ma_loaisp')
+    ten_loaisp = models.CharField(max_length=150, db_column='ten_loaisp')
 
     class Meta:
         db_table = 'LoaiSP'
         managed = False
-        app_label = 'menu'
 
     def __str__(self):
         return self.ten_loaisp
@@ -22,25 +21,24 @@ class SanPham(models.Model):
     ]
 
     ma_sp = models.AutoField(primary_key=True, db_column='ma_sp')
-    ten_sp = models.CharField(max_length=200, db_column='TenSP')
-    gia = models.DecimalField(max_digits=12, decimal_places=2, db_column='Gia')
+    ten_sp = models.CharField(max_length=200, db_column='ten_sp')
+    gia = models.DecimalField(max_digits=12, decimal_places=2, db_column='gia')
     trang_thai = models.CharField(
         max_length=10,
         default='Còn',
         choices=TRANG_THAI_CHOICES,
-        db_column='TrangThai'
+        db_column='trang_thai'
     )
     ma_loaisp = models.ForeignKey(
         LoaiSP,
         on_delete=models.CASCADE,
-        db_column='MaLoaiSP',
+        db_column='ma_loaisp',
         related_name='san_pham'
     )
 
     class Meta:
         db_table = 'SanPham'
         managed = False
-        app_label = 'menu'
 
     def __str__(self):
         return f"{self.ten_sp} - {self.get_trang_thai_display()}"
