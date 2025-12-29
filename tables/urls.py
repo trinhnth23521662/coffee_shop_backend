f6d56c5c1090baf1181b205420fe7b94ea322a89
thudinh_23521534 <23521534@gm.uit.edu.vn>
2025-12-29 16:49:10 +0700

    Update urls.py

--- tables/urls.py
+++ tables/urls.py
@@ -1,6 +1,33 @@
 from django.urls import path
-from .views import AdminReportAPIView
+from .views import (
+    AdminReportAPIView,
+    TableListAPIView,
+    CreateTableAPIView,
+    UpdateTableAPIView,
+    DeleteTableAPIView,
+    TableDetailAPIView,
+    UpdateTableStatusAPIView
+)
 
 urlpatterns = [
     path('admin/reports/', AdminReportAPIView.as_view()),
+
+    # Danh sách bàn
+    path('', TableListAPIView.as_view()),
+
+    # Tạo bàn
+    path('create/', CreateTableAPIView.as_view()),
+
+    # Xem chi tiết bàn
+    path('<int:ma_ban>/', TableDetailAPIView.as_view()),
+
+    # Sửa bàn
+    path('<int:ma_ban>/update/', UpdateTableAPIView.as_view()),
+
+    # Cập nhật trạng thái bàn
+    path('<int:ma_ban>/status/', UpdateTableStatusAPIView.as_view()),
+
+    # Xóa bàn
+    path('<int:ma_ban>/delete/', DeleteTableAPIView.as_view()),
 ]
+
