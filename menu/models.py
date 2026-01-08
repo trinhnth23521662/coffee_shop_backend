ef70012ec9c8df96fedab88ea681b422c7fb34e3
trinhnth23521662 <23521662@gm.uit.edu.vn>
2026-01-08 23:25:51 +0700

    Add files via upload

--- menu/models.py
+++ menu/models.py
@@ -2,43 +2,23 @@ from django.db import models
 
 
 class LoaiSP(models.Model):
-    ma_loaisp = models.AutoField(primary_key=True, db_column='ma_loaisp')
-    ten_loaisp = models.CharField(max_length=150, db_column='ten_loaisp')
+    ma_loaisp = models.AutoField(primary_key=True)
+    ten_loaisp = models.CharField(max_length=150)
 
     class Meta:
         db_table = 'LoaiSP'
         managed = False
-
-    def __str__(self):
-        return self.ten_loaisp
+        app_label = 'menu'
 
 
 class SanPham(models.Model):
-    TRANG_THAI_CHOICES = [
-        ('Còn', 'Còn hàng'),
-        ('Hết', 'Hết hàng'),
-        ('Ngừng', 'Ngừng kinh doanh'),
-    ]
-
-    ma_sp = models.AutoField(primary_key=True, db_column='ma_sp')
-    ten_sp = models.CharField(max_length=200, db_column='ten_sp')
-    gia = models.DecimalField(max_digits=12, decimal_places=2, db_column='gia')
-    trang_thai = models.CharField(
-        max_length=10,
-        default='Còn',
-        choices=TRANG_THAI_CHOICES,
-        db_column='trang_thai'
-    )
-    ma_loaisp = models.ForeignKey(
-        LoaiSP,
-        on_delete=models.CASCADE,
-        db_column='ma_loaisp',
-        related_name='san_pham'
-    )
+    ma_sp = models.AutoField(primary_key=True)
+    ten_sp = models.CharField(max_length=200)
+    gia = models.DecimalField(max_digits=12, decimal_places=2)
+    trang_thai = models.CharField(max_length=10, default='Còn')
+    ma_loaisp = models.ForeignKey(LoaiSP, on_delete=models.CASCADE, db_column='ma_loaisp')
 
     class Meta:
         db_table = 'SanPham'
         managed = False
-
-    def __str__(self):
-        return f"{self.ten_sp} - {self.get_trang_thai_display()}"
+        app_label = 'menu'
--- menu/urls.py
+++ menu/urls.py
@@ -1,21 +1,18 @@
 from django.urls import path
 from .views import (
-    CategoryListAPIView,
-    CategoryDetailAPIView,
-    ProductListAPIView,
-    ProductDetailAPIView,
+    LoaiSPAPIView,
+    LoaiSPDetailAPIView,
+    SanPhamAPIView,
+    SanPhamDetailAPIView,
     PublicMenuAPIView
 )
 
 urlpatterns = [
-    # Loại sản phẩm
-    path('categories/', CategoryListAPIView.as_view()),
-    path('categories/<int:ma_loaisp>/', CategoryDetailAPIView.as_view()),
+    path('loaisp/', LoaiSPAPIView.as_view()),
+    path('loaisp/<int:ma_loaisp>/', LoaiSPDetailAPIView.as_view()),
 
-    # Sản phẩm (quản lý)
-    path('products/', ProductListAPIView.as_view()),
-    path('products/<int:ma_sp>/', ProductDetailAPIView.as_view()),
+    path('sanpham/', SanPhamAPIView.as_view()),
+    path('sanpham/<int:ma_sp>/', SanPhamDetailAPIView.as_view()),
 
-    # Menu công khai (cho khách hàng)
-    path('menu/', PublicMenuAPIView.as_view()),
+    path('', PublicMenuAPIView.as_view()),
 ]
