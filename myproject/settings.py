58d25ef71af79d4acdcc71da64e8837e09c71947
trinhnth23521662 <23521662@gm.uit.edu.vn>
2026-01-08 23:26:25 +0700

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'tables',
     'orders',
     'promotions',
-    # 'reports',
+    'reports',
     'admin_app',
 ]
 
@@ -145,6 +145,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 #AUTH_USER_MODEL = 'accounts.User'
